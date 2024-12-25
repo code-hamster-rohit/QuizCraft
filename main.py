c67a32cc0d4d915869d8960d12c8ac317ce79861
code-hamster-rohit <21mc3013@rgipt.ac.in>
2024-12-26 02:44:43 +0530

    changing main

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.auth.auth import Auth
 import uvicorn
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["Content-Disposition"]
+)
 
 app.include_router(Auth().router, prefix='/auth', tags=['Auth'])
 
