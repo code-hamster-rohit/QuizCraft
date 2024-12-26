468898ec6e71259aa8777f7012a6148b1cdfae14
code-hamster-rohit <21mc3013@rgipt.ac.in>
2024-12-26 19:10:46 +0530

    changing auth

--- operations/rules.py
+++ operations/rules.py
@@ -5,8 +5,12 @@ class Rules:
         return str(client[database][collection].insert_one(data).inserted_id)
     
     @staticmethod
-    def get(data: dict, client: object, database: str, collection: str) -> dict:
-        return client[database][collection].find_one(data)
+    def get(query: dict, client: object, database: str, collection: str) -> dict:
+        return client[database][collection].find_one(query)
+    
+    @staticmethod
+    def get_all(query: dict, client: object, database: str, collection: str) -> list:
+        return list(client[database][collection].find(query))
 
     @staticmethod
     def update(query: dict, data: dict, client: object, database: str, collection: str) -> dict:
