456e06e6803fda67d59efa967c1baeb5a3b6f682
code-hamster-rohit <21mc3013@rgipt.ac.in>
2024-12-26 19:19:04 +0530

    changing auth

--- routes/auth/auth.py
+++ routes/auth/auth.py
@@ -33,7 +33,7 @@ class Auth:
     
     
     async def __signup(self, first_name: str = Form(...), last_name: str = Form(...), email: str = Form(...), password: str = Form(...), use_type: str = Form(...), otp: str = Form(...)):
-        if otp == Rules.get_all({'email': email}, self.client, 'AUTH', 'OTPS')[-1]['otp']:
+        if otp == str(Rules.get_all({'email': email}, self.client, 'AUTH', 'OTPS')[-1]['otp']):
             data = {'_id': ObjectId(), 'first_name': first_name, 'last_name': last_name, 'email': email, 'password': AuthUtils.get_hashed_password(password), 'use_type': use_type, 'is_active': True, 'date_joined': datetime.now()}
             operation_id = Rules.add(data, self.client, 'AUTH', 'USERS')
             access_token, refresh_token = AuthUtils.create_access_token(email), AuthUtils.create_refresh_token(email)
@@ -52,7 +52,7 @@ class Auth:
             return {'status_code': 404, 'message': 'User does not exist, try signing up'}
 
     async def __login(self, email: str = Form(...), password: str = Form(...), otp: str = Form(...)):
-        if otp == Rules.get_all({'email': email}, self.client, 'AUTH', 'OTPS')[-1]['otp']:
+        if otp == str(Rules.get_all({'email': email}, self.client, 'AUTH', 'OTPS')[-1]['otp']):
             query = {'email': email}
             data = Rules.get(query, self.client, 'AUTH', 'USERS')
             if AuthUtils.verify_password(password, data['password']):
@@ -65,7 +65,7 @@ class Auth:
             return {'status_code': 404, 'data': {}, 'message': 'Invalid OTP'}
     
     async def __forgot_password(self, email: str = Form(...), otp: str = Form(...), new_password: str = Form(...)):
-        if otp == Rules.get_all({'email': email}, self.client, 'AUTH', 'OTPS')[-1]['otp']:
+        if otp == str(Rules.get_all({'email': email}, self.client, 'AUTH', 'OTPS')[-1]['otp']):
             Rules.update({'email': email}, {'password': new_password}, self.client, 'AUTH', 'USERS')
             return {'status_code': 200, 'message': 'Password Updated Successfully'}
         else:
